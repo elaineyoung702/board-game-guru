c174f44885c00c351da715a31a062bdff2cb9918
Elaine Young <elaineyoung702@gmail.com>
2019-08-15 16:47:32 -0700

    added login fields into base.html and to homepage. will probably get rid of the login.html file

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template, redirect, flash
 # from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
-from model import connect_to_db, db, BoardGame
+from model import connect_to_db, db, BoardGame, User, Favorite
 
 
 
@@ -12,6 +12,7 @@ app.secret_key = "s00persekret"
 
 app.jinja_env.undefinted = StrictUndefined
 
+
 @app.route('/')
 def index():
     """Homepage."""
@@ -19,19 +20,18 @@ def index():
     return render_template("homepage.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST"])
 def show_login_page():
     """Show Login Form."""
 
+    user_list = User.query.all()
+
+    email = request.form.get("email")
+    password = request.form.get('password')
+
+    print (f'email = {email}, password = {password}')
+
     return render_template("base.html")
-
-
-
-@app.route('/favorites')
-def show_favorites():
-    """Show User's Favorite Board Games."""
-
-    return "<html><body>Placeholder</body></html>"
 
 
 @app.route('/boardgame/<bg_id>')
@@ -43,7 +43,6 @@ def show_boardgame_info(bg_id):
     return render_template('boardgame.html', boardgame=boardgame)
 
 
-
 @app.route('/database')
 def show_database():
     """Show Board Game Database."""
@@ -53,13 +52,18 @@ def show_database():
     return render_template('database.html', bg_obj_list=bg_obj_list)
 
 
+@app.route('/favorites')
+def show_favorites():
+    """Show User's Favorite Board Games."""
+
+    return render_template('base.html')
+
+
 @app.route('/search')
 def show_search_form():
     """Show Search Form."""
 
-    return "<html><body>Placeholder</body></html>"
-
-
+    return render_template('base.html')
 
 
 
