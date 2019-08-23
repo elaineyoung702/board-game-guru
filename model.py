2eb62a63444295534fb687a9c40287f85c24c238
Elaine Young <elaineyoung702@gmail.com>
2019-08-23 16:51:11 -0700

    created 2 more tables. building new approved feature for project of allowing users to add tags/categories to a board game

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
 
-    name = db.Column(db.String(50), nullable=True)
-    email = db.Column(db.String(50), nullable=True)
-    password = db.Column(db.String(64), nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
 
     favorites = db.relationship("BoardGame", secondary="favorites",
                                     backref="users")
@@ -32,20 +32,20 @@ class BoardGame(db.Model):
 
     bg_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    bg_name = db.Column(db.String, nullable=True)
+    bg_name = db.Column(db.String, nullable=False)
     thumbnail_url = db.Column(db.String, nullable=False, default='/static/image/placeholder.png')
     image_url = db.Column(db.String, nullable=False)
     description = db.Column(db.Text, nullable=False)
 
-    playtime = db.Column(db.Integer, nullable=True)
+    playtime = db.Column(db.Integer, nullable=False)
     min_time = db.Column(db.Integer, nullable=False)
     max_time = db.Column(db.Integer, nullable=False)
 
-    year_published = db.Column(db.Integer, nullable=True) #maybe do datetime for learning!
+    year_published = db.Column(db.Integer, nullable=False) #maybe do datetime for learning!
 
-    min_players = db.Column(db.Integer, nullable=True)
-    max_players = db.Column(db.Integer, nullable=True)
-    suggested_players = db.Column(db.Integer, nullable=True)
+    min_players = db.Column(db.Integer, nullable=False)
+    max_players = db.Column(db.Integer, nullable=False)
+    suggested_players = db.Column(db.Integer, nullable=False)
 
     designer = db.Column(db.String(100), nullable=False)
     publisher = db.Column(db.String, nullable=False)
@@ -73,6 +73,40 @@ class Favorite(db.Model):
         """Provide helpful Favorite info when printed."""
 
         return f"<Favorite fav_id={self.fav_id}, user_id={self.user_id}, bg_id={self.bg_id}>"
+
+
+class BgTag(db.Model):
+    """A collection of a board games's tags/categories."""
+
+    __tablename__ = "bg_tags"
+    __table_args__ = (db.UniqueConstraint('user_id', 'bg_id', 'tag_id'),)
+
+    tagged_bg_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    bg_id = db.Column(db.Integer, db.ForeignKey('boardgames.bg_id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.tag_id'))
+
+
+    def __repr__(self):
+        """Provide helpful Favorite info when printed."""
+
+        return f"<BgTag tagged_bg_id={self.tagged_bg_id}, tag_id={self.tag_id}, bg_id={self.bg_id}, user_id={self.user_id}>"
+
+
+class Tag(db.Model):
+    """A list of tags that can be applied to a board game."""
+
+    tag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    tag_name = db.Column(db.String(75), nullable=False)
+
+
+    def __repr__(self):
+        """Provide helpful Favorite info when printed."""
+
+        return f"<Tag tag_id={self.tag_id}, tag_name={self.tag_name}>"
+
+
 
 
 #########################################################################
