ded96e18b23955435558d0ff403b280e384be005
Elaine Young <elaineyoung702@gmail.com>
2019-09-04 14:49:33 -0700

    refactored and reformatted / route to new-favorite. added spacing for easier readability. added into model.py db.relationships to clean up query syntax in server.py and reduce number of queries when rendering page

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, flash
 from flask import render_template, redirect, jsonify
-# from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 from model import connect_to_db, db, BoardGame, User, Favorite, Tag, BgTag
 
@@ -12,207 +11,236 @@ app.secret_key = "s00persekret"
 app.jinja_env.undefinted = StrictUndefined
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Display Homepage."""
 
     return render_template("homepage.html")
 
 
-@app.route('/login', methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login_user():
     """Login User or Redirect to Registration Form."""
 
-    email = request.form.get("email")   #get email provided in form
-    password = request.form.get('password') #get passwork provided in form
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     try:
-        user = db.session.query(User).filter(User.email==email, User.password==password).first() # check this syntax
-        session['user_id'] = user.user_id   #set session user_id
-        session['user_name'] = user.name.title()
-        session['email'] = user.email   #set session email (maybe get rid of this?)
+        user = User.query.filter(User.email == email,
+                                 User.password == password
+                                 ).first()
+
+        session["user_id"] = user.user_id   #set session[user]
+        session["user_name"] = user.name.title()
+        session["email"] = user.email
+
         print(f"SESSION USER EMAIL: {session['email']}") #Debugging prints
         print(f"Session User ID: {session['user_id']}") #Debugging prints
-        return redirect('/favorites')
+
+        return redirect("/favorites")
+
     except AttributeError:
         print("no login") #Debugging prints
+
         return render_template("register.html") ### REDIRECT THIS
 
 
-@app.route('/register', methods=["POST"])
+@app.route("/register", methods=["POST"])
 def register_new_user():
     """Create New User Account."""
 
-    name = request.form.get('name')
+    name = request.form.get("name")
     email = request.form.get("email")   #get email provided in form
-    password = request.form.get('password')
+    password = request.form.get("password")
 
-    if (email,) in db.session.query(User.email).all():
+    if User.query.filter(User.email == email).first():
         print("there is already an account with this email address")
-        return render_template('register.html')### REDIRECT THIS
+        flash("An account already exists for this email address.")
+
+        return redirect("/")
+
     else:
         user = User(name=name, email=email, password=password)
+
         db.session.add(user)
         db.session.commit()
-        user = db.session.query(User).filter(User.email==email, User.password==password).first() # check this syntax
-        session['user_id'] = user.user_id   #set session user_id
-        session['user_name'] = user.name.title()
-        session['email'] = user.email 
-        return render_template('favorites.html') ### REDIRECT THIS
+
+        session["user_id"] = user.user_id   #set session user_id
+        session["user_name"] = user.name.title()
+        session["email"] = user.email 
+
+        return render_template("favorites.html") ### REDIRECT THIS
 
 
-@app.route('/logout', methods=["POST"])
+@app.route("/logout", methods=["POST"])
 def logout_user():
     """Log User Out and Clear Session Data."""
 
     session.clear()
     print(session)
 
-    return render_template('homepage.html')
+    return redirect("/")
 
 
-@app.route('/boardgame/<bg_id>')
+@app.route("/boardgame/<bg_id>")
 def show_boardgame_info(bg_id):
     """Show Board Game Info Page."""
 
-    boardgame = BoardGame.query.get(bg_id) # get bg object to pass into jinja
-    tags = Tag.query.all() # get all tags to pass into jinja and for interation
+    boardgame = BoardGame.query.options(db.joinedload("bg_tags")).get(bg_id) 
+                # get bg object to pass into jinja
+    all_tags = Tag.query.all() # get all tags to pass into jinja and for interation
     tag_dict = {} #creating dict to pass into jinja with bg/tag counts
 
-    for tag in tags: # for each tag, get tag_id and count for matching bg_id
+    for tag in all_tags: # for each tag, get tag_id and count for matching bg_id
         tag_id = tag.tag_id
         # print(tag_id)
-        count = BgTag.query.filter(BgTag.bg_id==bg_id,BgTag.tag_id==tag_id).count()
+        count = boardgame.count_tags(tag_id)
         tag_dict[tag_id] = count # add to dict for passing into jinja for displaying
 
-    # print(tag_dict)
-
     if session:
-        user = db.session.query(User).filter_by(user_id=session['user_id']).one()
+        user = User.query.get(session["user_id"])
+
         user_id = user.user_id
-        tag_check = db.session.query(BgTag.tag_id).filter(BgTag.user_id==user_id, BgTag.bg_id==bg_id).all()
-        if tags:
-            bg_tagged = []
-            for tag_tuple in tag_check:
-                (tagged, ) = tag_tuple
-                bg_tagged.append(tagged)
-        fav_exist = db.session.query(Favorite).filter(Favorite.user_id==user_id, Favorite.bg_id==bg_id).first()
-        print(fav_exist)
+        users_tags = set(user.get_tags_by_bg(bg_id))
+
+        if all_tags:
+            for tag in all_tags:
+                tag.tagged_by_user = tag in users_tags
+
+        fav_exist = Favorite.query.filter(Favorite.user_id == user_id,
+                                          Favorite.bg_id == bg_id).first()
     
-        return render_template('boardgame.html', boardgame=boardgame, user=user, tags=tags, tag_dict=tag_dict, bg_tagged=bg_tagged, fav_exist=fav_exist)
+        return render_template("boardgame.html",
+                               boardgame=boardgame,
+                               user=user,
+                               tags=all_tags,
+                               tag_dict=tag_dict,
+                               fav_exist=fav_exist)
+    
     else:
-        return render_template('boardgame.html', boardgame=boardgame, tags=tags, tag_dict=tag_dict)
+
+        return render_template("boardgame.html",
+                               boardgame=boardgame,
+                               tags=all_tags,
+                               tag_dict=tag_dict)
 
 
-@app.route('/database')
+@app.route("/database")
 def show_database():
     """Show Board Game Database."""
-    i = 0
 
-    bg_obj_list = BoardGame.query.order_by(BoardGame.bg_id.desc()).offset(i).limit(50).all()
-    # bg_obj_list = BoardGame.query.order_by(BoardGame.bg_id.desc()).all()
-    if 'user_id' in session:
-        user = User.query.get(session['user_id'])
+    bg_obj_list = BoardGame.query.order_by(BoardGame.bg_id.desc()).limit(50).all()
+
+    if "user_id" in session:
+        user = User.query.get(session["user_id"])
         user_id = user.user_id
-        return render_template('database.html', bg_obj_list=bg_obj_list, user_id=user_id)
 
-    return render_template('database.html', bg_obj_list=bg_obj_list)
+        return render_template("database.html",
+                               bg_obj_list=bg_obj_list,
+                               user_id=user_id)
 
-# ######################################
+    return render_template("database.html",
+                           bg_obj_list=bg_obj_list)
 
-@app.route('/new-favorite', methods=["POST"])
+
+@app.route("/new-favorite", methods=["POST"])
 def check_favorites_table():
 
     try:
         bg_id = request.form.get("bg_id")
-        user = User.query.get(session['user_id'])
+        user = User.query.get(session["user_id"])
         user_id = user.user_id
+
         fav_exist = db.session.query(Favorite).filter(Favorite.user_id==user_id, Favorite.bg_id==bg_id).first()
-        # print(fav_exist)
+
         if fav_exist:
             fav_id = fav_exist.fav_id
-            # print(tagged_bg_id)
             Favorite.query.filter(Favorite.fav_id==fav_id).delete()
             db.session.query(Favorite).filter(Favorite.user_id==user_id, Favorite.bg_id==bg_id).first()
             db.session.commit()
-            return jsonify( {"favorited" : "false"}) #how do we wanna return from this?
+
+            return jsonify( {"favorited" : "false"})
+
         else:
-            bg_id = request.form.get('bg_id')
+            bg_id = request.form.get("bg_id")
             bg = BoardGame.query.get(bg_id)
             user.favorites.append(bg)
             db.session.commit()
-            return jsonify( {"favorited" : "true"}) #how do we wanna return from this?
+
+            return jsonify( {"favorited" : "true"})
+
     except KeyError:
-        return render_template('register.html')
+
+        return render_template("register.html")
 
 
-@app.route('/favorites')
+@app.route("/favorites")
 def show_user_favorites():
-    """Show User's Favorite Board Games."""
+    """Show User"s Favorite Board Games."""
 
-    if 'user_id' in session:
-        user = User.query.get(session['user_id'])
+    if "user_id" in session:
+        user = User.query.get(session["user_id"])
         bg_obj_list = user.favorites
 
-        return render_template('favorites.html',bg_obj_list=bg_obj_list)
+        return render_template("favorites.html",bg_obj_list=bg_obj_list)
+
     else:
-        return render_template('register.html')
+
+        return render_template("register.html")
 
 
-@app.route('/search-form')
+@app.route("/search-form")
 def show_search_form():
     """Show Search Form."""
 
     designer_set = set()
 
-    bg_list = BoardGame.query.filter(BoardGame.designer !='(Uncredited)').all()
+    bg_list = BoardGame.query.filter(BoardGame.designer !="(Uncredited)").all()
+
     for item in bg_list:
         designer = item.designer
         designer_set.add(designer)
+
     designer_list = (sorted(list(designer_set)))
 
-    return render_template('search.html', designer_list=designer_list)
+    return render_template("search.html", designer_list=designer_list)
 
 
-@app.route('/bg-tagged', methods=['POST'])
+@app.route("/bg-tagged", methods=["POST"])
 def tag_a_board_game():
 
     tag_id = request.form.get("tag_id")
-    # print(tag_id)
-    user_id = session['user_id']
-    # print(user_id)
+    user_id = session["user_id"]
     bg_id = request.form.get("bg_id")
-    # print(bg_id)
 
     bg_exist = db.session.query(BgTag).filter(BgTag.tag_id==tag_id, BgTag.user_id==user_id, BgTag.bg_id==bg_id).first()
-    # print(bg_exist)
 
     if bg_exist:
         tagged_bg_id = bg_exist.tagged_bg_id
-        # print(tagged_bg_id)
         BgTag.query.filter(BgTag.tagged_bg_id==tagged_bg_id).delete()
         db.session.query(BgTag).filter(BgTag.tag_id==tag_id, BgTag.user_id==user_id, BgTag.bg_id==bg_id).first()
         db.session.commit()
+
         return jsonify( {"deleted" : "true", "tag_id" : tag_id })
+
     else:
         bgtag = BgTag(user_id=user_id, bg_id=bg_id, tag_id=tag_id)
         db.session.add(bgtag)
         db.session.commit()
-
-        print(f'WOOOOO! Added {bgtag}')
+        print(f"Added {bgtag}")
 
         return jsonify( {"deleted" : "false", "tag_id" : tag_id })
 
 
-@app.route('/results')
+@app.route("/results")
 def show_results():
     """Show User Search Results Based on Inputs from Search Form."""
 
-    bg_name = request.args.get('bg_name')
-    num_players = request.args.get('num_players')
-    playtime = request.args.get('playtime')
-    designer = request.args.get('designer')
-    publisher = request.args.get('publisher')
+    bg_name = request.args.get("bg_name")
+    num_players = request.args.get("num_players")
+    playtime = request.args.get("playtime")
+    designer = request.args.get("designer")
+    publisher = request.args.get("publisher")
 
     results = []
 
@@ -232,65 +260,69 @@ def show_results():
         des_results = get_by_designer(designer)
         results.extend(des_results)
 
-    return render_template('results.html', results=results)
+    return render_template("results.html", results=results)
 
 
 def get_by_bg_name(bg_name):
     bg_name = bg_name.title().replace(" ", "%")
-    bg_name_cat = f'%{bg_name}%'
-    print(bg_name_cat)
+    bg_name_cat = f"%{bg_name}%"
     name_search_results = BoardGame.query.filter(BoardGame.bg_name.like(f"{bg_name_cat}")).all()
-    print (f'BG Name Match: {name_search_results}')
+    print (f"BG Name Match: {name_search_results}")
+
     return name_search_results
 
 def get_by_num_players(num_players):
     best_fit = BoardGame.query.filter(BoardGame.suggested_players == num_players).all()
-    print(f'Num Player Best Fit: {best_fit}')
+    print(f"Num Player Best Fit: {best_fit}")
 
     if best_fit:
+
         return best_fit
+
     else:
         match_players = BoardGame.query.filter(BoardGame.min_players <= num_players,
             BoardGame.max_players >= num_players).all()
-        print(f'Other Matches: {match_players}')
+        print(f"Other Matches: {match_players}")
+
         return match_players
 
 def get_by_playtime(time):
     best_fit = BoardGame.query.filter(BoardGame.playtime == time).all()
-    print(f'Playtime Best Fit: {best_fit}')
+    print(f"Playtime Best Fit: {best_fit}")
+
     if best_fit:
+
         return best_fit
+
     else:
         match_playtime = BoardGame.query.filter(BoardGame.min_time <= time,
                 BoardGame.max_time >= time).all()
-        print(f'Other Matches: {match_playtime}')
+        print(f"Other Matches: {match_playtime}")
+
         return match_playtime
 
 def get_by_designer(designer):
     match = BoardGame.query.filter(BoardGame.designer == designer).all()
+
     if match:
-        print(match)
+
         return match
+
     else:
         print("no match")
-        return redirect('/search-form')
+
+        return redirect("/search-form")
 
 def get_by_publisher(publisher):
     publisher = publisher.title().replace(" ", "%")
-    publisher_cat = f'%{publisher}%'
-    print(publisher_cat)
+    publisher_cat = f"%{publisher}%"
     name_search_results = BoardGame.query.filter(BoardGame.publisher.like(f"{publisher_cat}")).all()
-    print (f'publisher Name Match: {name_search_results}')
+    print (f"publisher Name Match: {name_search_results}")
+    
     return name_search_results
 
 
-
-###### WIP refactor designer dropdown to be input text with SQL LIKE query because the dropdown is out of control
-        #######if time permits, then try to create an auto complete function
-
 ##############################################################
-
-
 
 if __name__ == "__main__":
     app.debug = True
@@ -298,7 +330,7 @@ if __name__ == "__main__":
 
     connect_to_db(app)
 
-    # DebugToolbarExtension(app)
+    from flask_debugtoolbar import DebugToolbarExtension; DebugToolbarExtension(app)
 
     app.run(port=5000, host="0.0.0.0")
 
